d3d5de13abef5fc40f45711a23ecfd515f10e281
Yurii Litvinov <yurii.litvinov@gmail.com>
2017-01-25 00:06:37 +0300

    Made attributes respect generalization

--- Repo/RepoImpl.fs
+++ Repo/RepoImpl.fs
@@ -7,32 +7,45 @@ open GraphMetametamodel
 type private RepoImpl () = 
     let repoGraph, classes = GraphMetametamodel.createM0Model ()
 
+    let followEdge edgePredicate node = 
+        repoGraph.OutEdges node
+        |> Seq.filter edgePredicate
+        |> Seq.exactlyOne
+        |> fun e -> e.Target
+
+    let attrType a = a |> followEdge (fun e -> match e.Tag with | Type _ -> true | _ -> false) |> fun n -> n.id
+
+    let attrValue attribute node = 
+        match attribute.name with
+        | "Name" -> node.name
+        | "Level" -> string node.level
+        | "Potency" -> string node.potency
+        | _ -> ""
+
+    let attributes node =
+        repoGraph.OutEdges node 
+        |> Seq.filter (fun e -> match e.Tag with | Attribute _ -> true | _ -> false)
+        |> Seq.map (fun e -> e.Target)
+        |> Seq.map (fun attr -> new AttributeInfo(attr.name, attrType attr, attrValue attr node))
+
+    let rec effectiveAttributes node =
+        repoGraph.OutEdges node
+        |> Seq.filter (fun e -> match e.Tag with | Generalization _ -> true | _ -> false)
+        |> Seq.map (fun e -> e.Target)
+        |> Seq.collect (fun parent -> effectiveAttributes parent)
+        |> Seq.append (attributes node)
+        |> Seq.toList
+
+    let toList attrSeq =
+        attrSeq |> Seq.fold (fun (acc : List<AttributeInfo>) attr -> acc.Add(attr); acc) (new List<_>())
+
     interface Repo.Repo with
         member this.ModelNodes () = 
             let metaclass x = if classes.ContainsKey(x) && classes.[x].name = "Attribute" 
                               then NodeType.Attribute
                               else NodeType.Node
-
-            let value n = 
-                None  // All attributes don't have values for now
-
-            let followEdge edgePredicate node = 
-                repoGraph.OutEdges node
-                |> Seq.filter edgePredicate
-                |> Seq.exactlyOne
-                |> fun e -> e.Target
-
-            let attrType a = a |> followEdge (fun e -> match e.Tag with | Type _ -> true | _ -> false) |> fun n -> n.id
-            
-            let attributes x =
-                repoGraph.OutEdges x 
-                |> Seq.filter (fun e -> match e.Tag with | Attribute _ -> true | _ -> false)
-                |> Seq.map (fun e -> e.Target)
-                |> Seq.map (fun n -> (n, value n))
-                |> Seq.map (fun (attr, v) -> new AttributeInfo(attr.name, attrType attr, ""))
-                |> Seq.fold (fun (acc : List<AttributeInfo>) attr -> acc.Add(attr); acc) (new List<_>())
-                
-            repoGraph.Vertices |> Seq.map (fun x -> new NodeInfo(x.id, x.name, metaclass x, attributes x))
+                            
+            repoGraph.Vertices |> Seq.map (fun x -> new NodeInfo(x.id, x.name, metaclass x, toList <| effectiveAttributes x))
 
         member this.ModelEdges () = 
             let edgeType = function
