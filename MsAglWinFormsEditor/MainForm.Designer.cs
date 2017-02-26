1308da21a021dd6a15a6c47c65fe2e1405c4599d
Yurii Litvinov <yurii.litvinov@gmail.com>
2017-02-27 00:52:55 +0300

    Minor styleguide changes

--- MsAglWinFormsEditor/MainForm.Designer.cs
+++ MsAglWinFormsEditor/MainForm.Designer.cs
@@ -30,33 +30,33 @@ namespace MsAglWinFormsEditor
         /// </summary>
         private void InitializeComponent()
         {
-            this.AttributeTable = new System.Windows.Forms.DataGridView();
+            this.attributeTable = new System.Windows.Forms.DataGridView();
             this.AttributeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.mainLayout = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            ((System.ComponentModel.ISupportInitialize)(this.AttributeTable)).BeginInit();
-            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.attributeTable)).BeginInit();
+            this.mainLayout.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // AttributeTable
+            // attributeTable
             // 
-            this.AttributeTable.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
-            this.AttributeTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.AttributeTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.attributeTable.AllowUserToAddRows = false;
+            this.attributeTable.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.attributeTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.attributeTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.AttributeName,
             this.Type,
             this.Value});
-            this.AttributeTable.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.AttributeTable.Location = new System.Drawing.Point(3, 370);
-            this.AttributeTable.Name = "AttributeTable";
-            this.AttributeTable.RowHeadersVisible = false;
-            this.AttributeTable.AllowUserToAddRows = false;
-            this.AttributeTable.Visible = false;
-            this.AttributeTable.Size = new System.Drawing.Size(239, 154);
-            this.AttributeTable.TabIndex = 1;
+            this.attributeTable.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.attributeTable.Location = new System.Drawing.Point(3, 370);
+            this.attributeTable.Name = "attributeTable";
+            this.attributeTable.RowHeadersVisible = false;
+            this.attributeTable.Size = new System.Drawing.Size(239, 154);
+            this.attributeTable.TabIndex = 1;
+            this.attributeTable.Visible = false;
             // 
             // AttributeName
             // 
@@ -79,25 +79,25 @@ namespace MsAglWinFormsEditor
             this.Value.ReadOnly = true;
             this.Value.Width = 70;
             // 
-            // tableLayoutPanel1
+            // mainLayout
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.79958F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.20042F));
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(958, 533);
-            this.tableLayoutPanel1.TabIndex = 2;
+            this.mainLayout.ColumnCount = 2;
+            this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.79958F));
+            this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.20042F));
+            this.mainLayout.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.mainLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainLayout.Location = new System.Drawing.Point(0, 0);
+            this.mainLayout.Name = "mainLayout";
+            this.mainLayout.RowCount = 1;
+            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainLayout.Size = new System.Drawing.Size(958, 533);
+            this.mainLayout.TabIndex = 2;
             // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.AttributeTable, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.attributeTable, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(710, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
@@ -112,23 +112,23 @@ namespace MsAglWinFormsEditor
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(958, 533);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.mainLayout);
             this.Name = "MainForm";
             this.Text = "MainForm";
-            ((System.ComponentModel.ISupportInitialize)(this.AttributeTable)).EndInit();
-            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.attributeTable)).EndInit();
+            this.mainLayout.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel mainLayout;
         private DataGridViewTextBoxColumn AttributeName;
         private DataGridViewTextBoxColumn Type;
         private DataGridViewTextBoxColumn Value;
         private TableLayoutPanel tableLayoutPanel2;
-        private DataGridView AttributeTable;
+        private DataGridView attributeTable;
     }
 }
 
