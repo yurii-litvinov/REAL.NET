ceb8a1684bb4eec82901b54361b1c03e80a5db9e
Yurii Litvinov <yurii.litvinov@gmail.com>
2017-03-26 11:18:02 +0300

    Uncommented test for model loading

--- tests/Repo.Tests/RepoTests.fs
+++ tests/Repo.Tests/RepoTests.fs
@@ -5,28 +5,26 @@ open FsUnit
 
 open Repo
 
-let mutable repo = RepoFactory.CreateRepo ()
+let mutable private repo = RepoFactory.CreateRepo () :?> RepoImpl
 
 [<SetUp>]
 let setup () =
-    repo <- RepoFactory.CreateRepo ()
+    repo <- RepoFactory.CreateRepo () :?> RepoImpl
 
-//[<Test>]
-//let ``Repo shall be able to load a model`` () =
-//    let modelLoader = ()
-//    repo.LoadModel modelLoader
-//    repo.Models |> should not' (be Empty)
-
+[<Test>]
+let ``Repo shall be able to load a model`` () =
+    let modelLoader = GraphMetametamodel () :> IModelLoader
+    modelLoader.LoadInto repo
 
 [<Test>]
 let ``Repo shall return some metamodel nodes`` () =
-    repo.MetamodelNodes () |> should not' (be Empty)
+    (repo :> IRepo).MetamodelNodes () |> should not' (be Empty)
 
 [<Test>]
 let ``Repo shall return some model nodes`` () =
-    repo.ModelNodes () |> should not' (be Empty)
+    (repo :> IRepo).ModelNodes () |> should not' (be Empty)
 
 [<Test>]
 let ``Repo shall contain at least ModelElement`` () =
-    repo.ModelNodes () |> Seq.filter (fun x -> x.name = "ModelElement") |> should not' (be Empty)
+    (repo :> IRepo).ModelNodes () |> Seq.filter (fun x -> x.name = "ModelElement") |> should not' (be Empty)
 
