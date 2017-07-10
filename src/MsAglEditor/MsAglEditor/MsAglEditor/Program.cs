63b127117c6a88eb98dabff78c63d791e851ea20
egorzainullin <egor.zainullin@outlook.com>
2017-07-10 16:32:12 +0300

    -minor changes

--- src/MsAglEditor/MsAglEditor/MsAglEditor/Program.cs
+++ src/MsAglEditor/MsAglEditor/MsAglEditor/Program.cs
@@ -17,7 +17,7 @@ namespace MsAglEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Editor());
         }
     }
 }
