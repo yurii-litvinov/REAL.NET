df4b284851ba04e2669b37662fbfdbe8e428bed6
egorzainullin <egor.zainullin@outlook.com>
2017-07-11 14:41:13 +0300

    -added UML diagram of Editor's components

---
+++ src/MsAglEditor/MsAglEditor/MsAglEditor/Editor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MsAglEditor
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+    }
+}
