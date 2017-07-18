9014142932f59d9ac838c6893502799166cd5b25
Sushentsev Denis <sushentsevds@gmail.com>
2017-07-18 16:49:29 +0300

    Implemented console code

---
+++ src/WPF_Editor/Models/Console/Console.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REAL.NET.Models
+{
+    public class Console
+    {
+        private ConsoleWindow messageConsole;
+        private ConsoleWindow errorConsole;
+
+        public Console(List<ConsoleWindow> list)
+        {
+            messageConsole = new ConsoleWindow();
+            errorConsole = new ConsoleWindow();
+        }
+    }
+}
--- src/WPF_Editor/ViewModels/EditorViewModel.cs
+++ src/WPF_Editor/ViewModels/EditorViewModel.cs
@@ -6,19 +6,36 @@
     using Repo;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
 
-    class EditorViewModel
+    class EditorViewModel : INotifyPropertyChanged
     {
         private IRepo repo;
+
+        public ConsoleWindow ErrorConsole { get; private set; }
+
+        public ConsoleWindow MessageConsole { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public ObservableCollection<NodeInfo> NodeCollection { get; }
+
         public ObservableCollection<EdgeInfo> EdgeCollection { get; }
+
+        public ObservableCollection<string> ErrorConsoleCollection { get; private set; }
+
+        public ObservableCollection<string> MessageConsoleCollection { get; private set; }
 
         public EditorViewModel()
         {
             repo = new FakeRepo();
             NodeCollection = new ObservableCollection<NodeInfo>(repo.ModelNodes("FakeModel"));
             EdgeCollection = new ObservableCollection<EdgeInfo>(repo.ModelEdges("FakeModel"));
+            this.MessageConsole = new ConsoleWindow();
+            this.ErrorConsole = new ConsoleWindow();
         }
+
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
     }
 }
