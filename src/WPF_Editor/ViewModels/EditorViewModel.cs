3c903daa0ccb897a8892c7870fba5cb91cf3eabc
Ivan <ivan.yarkov@outlook.com>
2017-07-18 13:43:16 +0300

    Changed WPF_Editor's path to src folder.

---
+++ src/WPF_Editor/ViewModels/EditorViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace REAL.NET.ViewModels
+{
+    using System;
+    using REAL.NET.Models;
+    using REAL.NET.Models.FakeRepo;
+    using Repo;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    class EditorViewModel
+    {
+        private IRepo repo;
+        public ObservableCollection<NodeInfo> NodeCollection { get; }
+        public ObservableCollection<EdgeInfo> EdgeCollection { get; }
+
+        public EditorViewModel()
+        {
+            repo = new FakeRepo();
+            NodeCollection = new ObservableCollection<NodeInfo>(repo.ModelNodes("FakeModel"));
+            EdgeCollection = new ObservableCollection<EdgeInfo>(repo.ModelEdges("FakeModel"));
+        }
+
+    }
+}
---
+++ src/WPF_Editor/Views/Editor.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace REAL.NET
+{
+    using System.Windows;
+    using REAL.NET.ViewModels;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            System.Console.WriteLine("Initialization");
+            InitializeComponent();
+            DataContext = new EditorViewModel();
+        }
+
+    }
+}
