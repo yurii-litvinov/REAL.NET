df454e5012f8ccfc246e90ddda9210ca27d4d557
egorzainullin <egor.zainullin@outlook.com>
2017-07-19 13:43:12 +0300

    added some forgotten files

---
+++ src/WPF_Editor/Models/Console/AppConsole.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REAL.NET.Models
+{
+    public class AppConsole
+    {
+        private Dictionary<string, ConsoleWindow> windows = new Dictionary<string, ConsoleWindow>();
+
+        public bool VisibilityStatus { get; private set; }
+
+        public AppConsole()
+        {
+            windows.Add("MessageConsole", new ConsoleWindow());
+            windows.Add("ErrorConsole", new ConsoleWindow());
+            VisibilityStatus = false;
+        }
+
+        public ConsoleWindow GetConsoleWindowByName(string name)
+        {
+            return windows[name];
+        }
+
+        public void NewMessageToConsoleWindowByName(string name, string message)
+        {
+            windows[name].NewMessage(message);
+        }
+
+        public void ShowConsole()
+        {
+            VisibilityStatus = true;
+        }
+
+        public void HideConsole()
+        {
+            VisibilityStatus = false;
+        }
+
+    }
+}
