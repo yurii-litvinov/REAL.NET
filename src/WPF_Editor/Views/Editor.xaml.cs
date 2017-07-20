9b1ebd98d0da17f6716237ce49b00187798e0ed4
egorzainullin <egor.zainullin@outlook.com>
2017-07-20 13:44:44 +0300

    downloaded new repo warning:this current version doesn't build

--- src/WPF_Editor/Views/Editor.xaml.cs
+++ src/WPF_Editor/Views/Editor.xaml.cs
@@ -12,19 +12,28 @@ namespace REAL.NET
     public partial class Editor : Window
     {
         IScene scene { get; }
+
         IPalette palette { get; }
+
+        private EditorViewModel viewModel;
 
         public Editor()
         {
             InitializeComponent();
-            DataContext = new EditorViewModel();
-            scene = ((EditorViewModel)DataContext).Mediator.Scene;
-            palette = ((EditorViewModel)DataContext).Mediator.Palette;
+            viewModel = new EditorViewModel();
+            DataContext = viewModel;
+            scene = viewModel.Mediator.Scene;
+            palette = viewModel.Mediator.Palette;
         }
 
         private void Scene_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             scene.HandleClick(sender, e);
+        }
+
+        private void OnHideShowButtonClick(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeConsoleVisibilityStatus();
         }
     }
 }
