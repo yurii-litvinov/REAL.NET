ed0f71de71f1fe1ddb14ca0109a269983e498452
MrsSima <goldenrybka@yandex.ru>
2017-10-19 18:38:04 +0300

    Reverting

--- tests/Repo.Tests/RobotsTestModelsTests.fs
+++ tests/Repo.Tests/RobotsTestModelsTests.fs
@@ -11,7 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License. *)
-
+ 
 module RobotsTestModelsTests
 
 open NUnit.Framework
@@ -25,27 +25,24 @@ let init () =
 [<Test>]
 let ``Generalizations shall be listed as edges in metamodel`` () =
     let repo = init ()
+
     let rawRepo = (repo :?> FacadeLayer.Repo).UnderlyingRepo
-    let infrastructure = InfrastructureSemanticLayer.InfrastructureSemantic rawRepo
+
     let infrastructureModel = repo.Model "InfrastructureMetamodel"
     let generalization = infrastructureModel.Nodes |> Seq.find (fun n -> n.Name = "Generalization")
     let model = repo.Model "RobotsMetamodel"
-
-    let rawEdges = model.Edges |> Seq.map (fun e -> (e :?> FacadeLayer.Element).UnderlyingElement :?> DataLayer.IEdge)
+    
+    let rawEdges = model.Edges |> Seq.map (fun e -> (e :?> FacadeLayer.Element).UnderlyingElement :?> DataLayer.IRelationship)
     let rawGeneralization = (generalization :?> FacadeLayer.Element).UnderlyingElement
     let rawGeneralizations = rawEdges |> Seq.filter (fun e -> e.Class = rawGeneralization)
     let someRawGeneralization = rawGeneralizations |> Seq.head
 
     someRawGeneralization.Class |> should sameAs rawGeneralization
 
-    infrastructure.Metamodel.IsGeneralization someRawGeneralization |> should be True
-    infrastructure.Metamodel.IsEdge someRawGeneralization |> should be True
+    InfrastructureSemanticLayer.InfrastructureMetamodel.isGeneralization rawRepo someRawGeneralization |> should be True
+    InfrastructureSemanticLayer.InfrastructureMetamodel.isRelationship rawRepo someRawGeneralization |> should be True
 
-    let someWrappedGeneralization =
-        model.Edges
-        |> Seq.find
-            (fun e -> (e :?> FacadeLayer.Edge).UnderlyingElement = (someRawGeneralization :> DataLayer.IElement))
-
+    let someWrappedGeneralization = model.Edges |> Seq.find (fun e -> (e :?> FacadeLayer.Edge).UnderlyingElement = (someRawGeneralization :> DataLayer.IElement))
     someWrappedGeneralization.Class |> should sameAs generalization
-
+    
     model.Edges |> Seq.filter (fun e -> e.Class = (generalization :> IElement)) |> should not' (be Empty)
