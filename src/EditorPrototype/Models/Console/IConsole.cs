e9ae2ffd70d4f475cfc97841e25839f2512c7dc9
egorzainullin <egor.zainullin@outlook.com>
2017-12-13 12:53:57 +0300

    added new files + some methods were written

--- src/EditorPrototype/Models/Console/IConsole.cs
+++ src/EditorPrototype/Models/Console/IConsole.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EditorPrototype.Models.Console
 {
     public interface IConsole
     {
+        event EventHandler<EventArgs> NewMessage;
+
+        event EventHandler<EventArgs> NewError;
+
+        IList<string> Messages { get; }
+
+        IList<string> Errors { get; }
+
+        void SendMessage(string message);
+
+        void ReportError(string error);
     }
 }
