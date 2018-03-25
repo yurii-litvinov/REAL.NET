67e4cec20d0a9e29494b3471393ce8a0d2178b2f
elizakuz <kuzminaeliz@gmail.com>
2018-03-25 14:04:00 +0300

    Revert "Generation project added"
    This reverts commit 37b9f44992d42a3d9f84d34bc0b98158d98519c0.

--- src/WpfEditor/Controls/Scene/StaticVertexConnectionPointForGH.cs
+++ src/WpfEditor/Controls/Scene/StaticVertexConnectionPointForGH.cs
@@ -1,25 +1,7 @@
-﻿/* Copyright 2017-2018 REAL.NET group
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. */
-
-namespace WpfEditor.Controls.Scene
+﻿namespace WpfEditor.Controls.Scene
 {
     using GraphX.Controls;
 
-    /// <summary>
-    /// Сontrol for the vertex connection point. It can be of two types: from which the edges can exit (IsSource = true) 
-    /// and into which they can enter.
-    /// </summary>
     public class StaticVertexConnectionPointForGH : StaticVertexConnectionPoint
     {
         public StaticVertexConnectionPointForGH()
@@ -28,9 +10,15 @@ namespace WpfEditor.Controls.Scene
         }
 
         public bool IsSource
-        { get; set; }
+        {
+            get;
+            set;
+        }
 
         public bool IsOccupied
-        { get; set; }
+        {
+            get;
+            set;
+        }
     }
 }
--- src/WpfEditor/Controls/Scene/VertexControlForGH.cs
+++ src/WpfEditor/Controls/Scene/VertexControlForGH.cs
@@ -1,25 +1,11 @@
-﻿/* Copyright 2017-2018 REAL.NET group
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. */
-
-namespace WpfEditor.Controls.Scene
+﻿namespace WpfEditor.Controls.Scene
 {
     using System.Windows;
     using System.Windows.Controls;
     using GraphX.Controls;
 
-    [TemplatePart(Name = "Source_vcproot", Type = typeof(Panel))]
-    [TemplatePart(Name = "Target_vcproot", Type = typeof(Panel))]
+    [TemplatePart(Name = "PART_source_vcproot", Type = typeof(Panel))]
+    [TemplatePart(Name = "PART_target_vcproot", Type = typeof(Panel))]
     public class VertexControlForGH : VertexControl
     {
         public VertexControlForGH(object vertexData, bool tracePositionChange = true, bool bindToDataObject = true)
@@ -35,8 +21,8 @@ namespace WpfEditor.Controls.Scene
         {
             base.OnApplyTemplate();
 
-            this.VCPSourceRoot = this.VCPSourceRoot ?? this.FindDescendant<Panel>("Source_vcproot");
-            this.VCPTargetRoot = this.VCPTargetRoot ?? this.FindDescendant<Panel>("Target_vcproot");
+            this.VCPSourceRoot = this.VCPSourceRoot ?? this.FindDescendant<Panel>("PART_source_vcproot");
+            this.VCPTargetRoot = this.VCPTargetRoot ?? this.FindDescendant<Panel>("PART_target_vcproot");
         }
     }
 }
