28e9736de715ee974d4d80a6793bf2586158b295
Denis <sushentsevds@gmail.com>
2018-05-02 13:55:17 +0300

    Added images to buttons in toolbar

--- src/WpfControlsLib/Controls/Toolbar/StandardButtonsAndMenus/SampleButtonsCollection.cs
+++ src/WpfControlsLib/Controls/Toolbar/StandardButtonsAndMenus/SampleButtonsCollection.cs
@@ -7,16 +7,16 @@
 
     public class SampleButtonsCollection
     {
-        private IConsole console;
-
         public SampleButtonsCollection(IConsole console)
         {
-            Action action1 = () => { console.SendMessage("action binded to button1 executed"); };
+            Action action1 = () => { console.SendMessage("Test undo button"); };
             var command1 = new Command(action1);
-            var button1 = new Button(command1, "sample button", "pic1");
-            Action action2 = () => { console.SendMessage("action binded to button2 executed"); };
+            var image1 = "pack://application:,,,/" + "View/Pictures/Toolbar/undo.png";
+            var button1 = new Button(command1, "Undo button", image1);
+            Action action2 = () => { console.SendMessage("Test redo button"); };
             var command2 = new Command(action2);
-            var button2 = new Button(command2, "sample button", "pic2");
+            var image2 = "pack://application:,,,/" + "View/Pictures/Toolbar/redo.png";
+            var button2 = new Button(command2, "Redo button", image2);
             this.SampleButtons.Add(button1);
             this.SampleButtons.Add(button2);
         }
