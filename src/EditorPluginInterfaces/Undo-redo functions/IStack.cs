b71e34a140d6d76491d99aa06f92c469b490ee4c
Denis <sushentsevds@gmail.com>
2018-05-19 14:25:19 +0300

    Implemented undo/redo stack

--- src/EditorPluginInterfaces/Undo-redo functions/IStack.cs
+++ src/EditorPluginInterfaces/Undo-redo functions/IStack.cs
@@ -1,7 +1,21 @@
-﻿using EditorPluginInterfaces.Toolbar;
+﻿/* Copyright 2017-2018 REAL.NET group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
 
 namespace EditorPluginInterfaces.Undo_redo_functions
 {
+    using EditorPluginInterfaces.Toolbar;
+
     /// <summary>
     /// Interface for stack of undo/redo commands.
     /// </summary>
@@ -18,7 +32,7 @@ namespace EditorPluginInterfaces.Undo_redo_functions
         bool IsRedoAvailable { get; }
 
         /// <summary>
-        /// Handling a command.
+        /// Handles a command.
         /// </summary>
         /// <param name="command">Command for handling.</param>
         void HandleCommand(ICommand command);
