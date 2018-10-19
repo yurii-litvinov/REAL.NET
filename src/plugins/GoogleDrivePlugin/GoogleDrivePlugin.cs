5719207f7dec3baa60171c24e5d2d2b02fa3af57
Ilya Balashov <ibalashov24@gmail.com>
2018-10-19 20:13:28 +0300

    Implement skeleton of GoogleDrivePlugin

---
+++ src/plugins/GoogleDrivePlugin/GoogleDrivePlugin.cs
@@ -0,0 +1,68 @@
+﻿namespace GoogleDrivePlugin
+{
+    using System;
+    using EditorPluginInterfaces;
+    using EditorPluginInterfaces.Toolbar;
+    using WpfControlsLib.Controls.Toolbar;
+
+    /// <summary>
+    /// Plugin for uploading models to and downloading them from Google Drive
+    /// </summary>
+    class GoogleDrivePlugin : IPlugin<PluginConfig>
+    {
+        /// <summary>
+        /// Toolbar in the top of the windows
+        /// </summary>
+        private IToolbar toolbar;
+
+        /// <summary>
+        /// Current opened model
+        /// </summary>
+        private IModel model;
+
+        /// <summary>
+        /// Editor console
+        /// </summary>
+        private IConsole console;
+
+        /// <summary>
+        /// Gets name of plugin
+        /// </summary>
+        public string Name => "Google Drive Integration Plugin";
+
+        /// <summary>
+        /// Sets plugin's configuration
+        /// </summary>
+        /// <param name="config">Configuration to set</param>
+        public void SetConfig(PluginConfig config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentException("This is not correct type of configuration");
+            }
+
+            this.toolbar = config.Toolbar;
+            this.model = config.Model;
+            this.console = config.Console;
+
+            this.PlaceButtonsOnToolbar();
+
+            this.console.SendMessage("Google Drive Plugin Successfully Launched");
+        }
+
+        private void PlaceButtonsOnToolbar()
+        {
+            var uploadButton = new Button(
+                null,
+                "Upload Model To Google Drive",
+                "pack://application:,,,/" + "GoogleDrivePlugin;component/ToolbarIcons/CloudUpload.png");
+            var downloadButton = new Button(
+                null,
+                "Download Model From Google Drive",
+                "pack://application:,,,/" + "GoogleDrivePlugin;component/ToolbarIcons/CloudDownload.png");
+
+            this.toolbar.AddButton(uploadButton);
+            this.toolbar.AddButton(downloadButton);
+        }
+    }
+}
---
+++ src/plugins/GoogleDrivePlugin/View/ImportDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GoogleDrivePlugin.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ImportDialog.xaml
+    /// </summary>
+    public partial class ImportDialog : Window
+    {
+        public ImportDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
