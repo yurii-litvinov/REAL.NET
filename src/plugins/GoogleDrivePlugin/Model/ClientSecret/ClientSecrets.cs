5c62aa785d107311638243caded538282ab11714
Ilya Balashov <ibalashov24@gmail.com>
2018-11-22 02:45:44 +0300

    Fix dummy auth credits

---
+++ src/plugins/GoogleDrivePlugin/Model/ClientSecret/ClientSecrets.cs
@@ -0,0 +1,9 @@
+﻿namespace GoogleDrivePlugin.Model.ClientSecret
+{
+    public static class ClientSecrets
+    {
+        public const string ClientID = "";
+
+        public const string ClientSecrted = "";
+    }
+}
