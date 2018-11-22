b3b8194089f4a4b5c1d594a2820dbb7aa6aa8bfc
Ilya Balashov <ibalashov24@gmail.com>
2018-11-22 04:19:32 +0300

    Fix .gitignore

---
+++ src/plugins/GoogleDrivePlugin/Model/ClientSecret/ClientSecretParams.cs
@@ -0,0 +1,11 @@
+﻿namespace GoogleDrivePlugin.Model.ClientSecret
+{
+    public static class ClientSecretParams
+    {
+        public const string ClientID = 
+            "479660120106-ddklk7ttebn95nq5sdqh9o6th89s26t7.apps.googleusercontent.com";
+
+        public const string ClientSecret = 
+            "IHDizvlABQgdGwod3hhYFTqL";
+    }
+}
