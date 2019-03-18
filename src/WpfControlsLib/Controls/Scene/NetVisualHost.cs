a0c8d58f9eaac216168326825a9ec58f569b8559
Katerina <katerina.kamkova@mail.ru>
2019-03-18 14:04:39 +0300

    Added forgotten file

---
+++ src/WpfControlsLib/Controls/Scene/NetVisualHost.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfControlsLib.Controls.Scene
+{
+    /// <summary>
+    /// Class that provides net for scene
+    /// </summary>
+    public class NetVisualHost : FrameworkElement
+    {
+        /// <summary>
+        /// Create a collection of child visual objects
+        /// </summary>
+        private readonly VisualCollection children;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="height">Height of the scene on the screen</param>
+        /// <param name="width">Width of the scene on the screen</param>
+        /// <param name="scale">The scale of net check</param>
+        /// <param name="point">The point that user presses on to move scene</param>
+        public NetVisualHost(double height, double width, int scale, Point point)
+        {
+            children = new VisualCollection(this)
+            {
+                CreateDrawingVisualLines(height, width, scale, point),
+            };
+        }
+
+        // Create a DrawingVisual that contains lines.
+        private DrawingVisual CreateDrawingVisualLines(double height, double width, int scale, Point point)
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            point = Geometry.RoundPosition(point, scale);
+
+            Pen drawingpen = new Pen(Brushes.Black, 1);
+            for (var i = point.Y; i < height + point.Y; i += scale)
+            {
+                drawingContext.DrawLine(drawingpen, new Point(-width + point.X, i), new Point(width + point.X, i));
+            }
+            for (var i = point.Y; i > -height + point.Y; i -= scale)
+            {
+                drawingContext.DrawLine(drawingpen, new Point(-width + point.X, i), new Point(width + point.X, i));
+            }
+            for (var i = point.X; i < width + point.X; i += scale)
+            {
+                drawingContext.DrawLine(drawingpen, new Point(i, -height + point.Y), new Point(i, height + point.Y));
+            }
+            for (var i = point.X; i > -width + point.X; i -= scale)
+            {
+                drawingContext.DrawLine(drawingpen, new Point(i, -height + point.Y), new Point(i, height + point.Y));
+            }
+
+            drawingContext.Close();
+            return drawingVisual;
+        }
+
+        /// <summary>
+        /// Provide a required override for the VisualChildrenCount property
+        /// </summary>
+        protected override int VisualChildrenCount => children.Count;
+
+        /// <summary>
+        /// Provide a required override for the GetVisualChild method
+        /// </summary>
+        protected override Visual GetVisualChild(int index)
+        {
+            if (index < 0 || index >= children.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return children[index];
+        }
+    }
+}
