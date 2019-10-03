10b7e4e74737da779becfddf8abfee4233a97842
yurii-litvinov <yurii.litvinov@gmail.com>
2019-10-03 15:44:58 +0300

    Installing fake-cli on the fly for travis

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 set -eu
 set -o pipefail
 
+export BUILD_PACKAGES=.fake
+export FAKE_CLI="$BUILD_PACKAGES/fake.exe"
+
+if [ -f "$%FAKE_CLI" ]; then
+  dotnet tool install fake-cli --tool-path ./$BUILD_PACKAGES
+fi
+
 dotnet restore build.proj
 dotnet fake build-core $@
 
