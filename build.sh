d7cd5976b35a78bb7b27e3e3f98d39972b0aa52f
yurii-litvinov <yurii.litvinov@gmail.com>
2019-10-03 16:00:14 +0300

    Fixed typo

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o pipefail
 export BUILD_PACKAGES=.fake
 export FAKE_CLI="$BUILD_PACKAGES/fake.exe"
 
-if [ -f "$%FAKE_CLI" ]; then
+if [ ! -f "$%FAKE_CLI" ]; then
   dotnet tool install fake-cli --tool-path ./$BUILD_PACKAGES
 fi
 
