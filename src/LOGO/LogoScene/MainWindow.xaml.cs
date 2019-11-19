4ff3529597f2e4ea3691bbd595b8144d61453424
egorzainullin <egor.zainullin@outlook.com>
2019-11-19 21:43:16 +0300

    -added scene

---
+++ src/LOGO/LogoScene/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LogoScene
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Canvas.SetLeft(turtleControl, 10);
+            //Canvas.SetTop(turtleControl, 10);
+            var a = turtleControl.ActualHeight;
+        }
+    }
+}
