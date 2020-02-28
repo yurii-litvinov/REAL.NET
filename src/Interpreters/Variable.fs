f50f138d8dba3393d5a45a2f7e151fa60affb8bd
egorzainullin <egor.zainullin@outlook.com>
2020-02-28 17:30:06 +0300

    -added comments

--- src/Interpreters/Variable.fs
+++ src/Interpreters/Variable.fs
@@ -3,64 +3,80 @@
 open System
 open Repo
 
+/// Represents primitive types, such as int, double and boolean
 type RegularType = 
     | Int of int
     | Double of double
     | Boolean of bool
 
+/// Represents a value, could be regular or complex
 type VariableValue = 
     | Regular of RegularType
     | Complex
 
+/// Mutability of variable.
 type VariableMutability =
     | Mutable = 0
     | Immutable = 1
 
+/// Block where variable was created or its absence. 
 type PlaceOfCreation = 
     | PlaceOfCreation of IModel option * IElement option
-    
+
+/// Meta info about variable, such as mutability, place of creation.    
 type MetaData = {Mutability: VariableMutability; PlaceOfCreation: PlaceOfCreation} with
     member this.IsMutable = this.Mutability = VariableMutability.Mutable 
 
+/// Represents a variable, contains name, value and meta.
 type Variable = { Name: string; Value: VariableValue; Meta: MetaData } with
+    /// Checks values of given variables for equality.
     static member (==) (x, y) = x.Value = y.Value
 
+    /// Checks variable's mutability.
     member this.IsMutable = this.Meta.IsMutable
 
 module PlaceOfCreation =
+    /// Creates none block PlaceOfCreation.
     let empty = PlaceOfCreation(None, None)
 
 module RegularType =
-    
+    /// Checks that types of given values are equal.
     let isTypesEqual xValue yValue = 
         match (xValue, yValue) with
         | (Int _, Int _) -> true
         | (Boolean _, Boolean _) -> true
         | (Double _, Double _) -> true
         | _ -> false // notice: not implemented for other types
-
+    
+    /// Creates wrapped int value.
     let createInt x = Int x
 
+    /// Creates wrapped double value.
     let createDouble x = Double x
     
+    /// Creates wrapped boolean value.
     let createBoolean x = Boolean x
 
 module VariableValue =
-    
+    /// Checks is given type regular.
     let isRegular (value: VariableValue) =
         match value with
         | Regular _ -> true
         | _ -> false
-
+    
+    /// Checks that types of given values are equal.
     let isTypesEqual xValue yValue =   
         match (xValue, yValue) with
         | (Regular xv, Regular yv) -> RegularType.isTypesEqual xv yv
         | _ -> new NotImplementedException() |> raise
-
+    
+    /// Creates wrapped int value.
     let createInt x = x |> RegularType.createInt |> Regular
 
+    /// Creates wrapped double value.
     let createDouble x = x |> RegularType.createDouble |> Regular
 
+    /// Creates wrapped boolean value.
     let createBoolean x = x |> RegularType.createBoolean |> Regular
 
 module MetaData =
